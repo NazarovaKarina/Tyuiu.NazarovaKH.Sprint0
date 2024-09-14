2547f0bba4c68414017ac98817c8c9020ae4f093
Tyuiu.NazarovaKKH@outlook.com <NazarovaKH@std.tyuiu.ru>
2024-09-14 22:55:47 +0300

    Task1

---
+++ Tyuiu.NazarovaKH.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.NazarovaKH.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Карина";
+            int age = 17;
+            Console.WriteLine("Здавствуйте меня зовут " + name + " мне " + age + " лет! ");
+            Console.ReadKey();
+        }
+    }
+}
